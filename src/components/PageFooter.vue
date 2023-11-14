ee366861abddf3b9cb8e441ca38400078a920ed2
valljoa <joan.vallat@divtec.ch>
2023-11-14 16:45:55 +0100

    Milieu partie 2

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+    &copy; 2023 - Supercell.com
+    <a href="mailto:joan.vallat@gmail.com">Contact</a>
+  </footer>
+</template>
+
+<style scoped lang="sass">
+footer
+  position: fixed
+  z-index: 999
+  bottom: 0
+  height: 60px
+  width: 100%
+  background-color: black
+  text-align: center
+  padding: 1rem
+  margin: 0
+
+  a
+    color: white
+
+</style>
